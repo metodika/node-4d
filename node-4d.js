f2ac43abcc0647819e99330b79470d8f64058908
Rob Laveaux <rob.laveaux@pluggers.nl>
2017-03-10 12:29:37 +0100

    Fixed bug parsing date values

--- node-4d.js
+++ node-4d.js
@@ -373,7 +373,7 @@ DbCommand.prototype.setRows = function( rows )
 				case 'VK_TIMESTAMP' :
 					// Is date- time value
 					var items = value.match( /(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})/ );
-					if( items.length >= 6 ) {
+					if( items && items.length >= 6 ) {
 						value = new Date( items[3] + '-' + items[2] + '-' + items[1] + ' ' + items[4] + ':' + items[5] );
 					} else {
 						value = null;
