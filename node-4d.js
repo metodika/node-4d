c5a7392c2eda33093ceeca066e504f6a67e6b765
Rob Laveaux <rob.laveaux@pluggers.nl>
2017-03-30 11:57:35 +0200

    Changed parser to use binary protocol instead of text-based protocol

--- node-4d.js
+++ node-4d.js
@@ -22,7 +22,7 @@ function DbConnection( options )
 	this.options =  options;
 	this.socket = null;
 	this.connected = false;
-	this.buffer = '';
+	this.buffer = Buffer.alloc( 0 );
 	this.error = 0;
 	this.errorMessage = '';
 	this.nextCommandID = 1;
@@ -31,11 +31,9 @@ function DbConnection( options )
 	this.queue = {};
 	
 	this.socket = new net.Socket();
-	this.socket.setEncoding( 'utf8' );
 	this.socket.setKeepAlive( true, 30 * 1000 );
 	
 	var self = this;
-	var command = null;
 	
 	this.socket.setTimeout( 180 * 1000, function () {
 		console.log( 'Node-4D: Socket timeout' );
@@ -52,37 +50,40 @@ function DbConnection( options )
 	
 	this.socket.on( 'data', function ( data ) {
 		// Append the incoming data to the buffer
-		// console.log( 'OnData:' + data );
-		self.buffer += data;
+		self.buffer = Buffer.concat( [ self.buffer, data ] );
 
 		while( packet = self.readPacket() ) {
-			// Parse the headers
-			var headers = parseHeaders( packet );
-			var rows = null;
 			
-			if( headers ) {
-				// We've received a new command, take the command from the command queue
-				command = self.queue[headers.commandID];
-			} else {
-				// We're receiving the rows part of a command
-				rows = parseRows( packet );
-			}
-			
-			if( command ) {
-				if( !command.result ) {
-					command.result = {};
+			if( packet.type == 'headers' ) {
+				// Take the command from the command queue
+				var command = self.queue[packet.commandID];
+				var [ headers, offset ] = command.parseHeaders( self.buffer );
+				if( offset > 0 ) {
+					self.buffer = self.buffer.slice( offset );
 				}
-				if( headers ) {
-					command.result.headers = headers;
-				}
-				if( headers && command.onDataHandler ) {
-					command.setResponseHeaders( headers );
+				command.setResponseHeaders( headers );
+				if( command.onDataHandler ) {
 					command.onDataHandler( headers );
 				}
-				if( rows && command.onDataHandler ) {
+				// Remove the command from the queue when it is completed
+				if( command.completed ) {
+					delete self.queue[command.commandID];
+				}
+			} else if( packet.type == 'data' ) {
+				// Take the command from the command queue
+				var command = self.queue[packet.commandID];
+				var [ rows, offset ] = command.parseRows( self.buffer );
+				if( offset > 0 ) {
+					self.buffer = self.buffer.slice( offset );
+				}
+				if( offset == 0 ) {
+					// The buffer contains a partial row, exit the loop and wait for more data
+					break;
+				}
+				command.setRows( rows );
+				if( command.onDataHandler ) {
 					command.onDataHandler( rows );
 				}
-				
 				// Remove the command from the queue when it is completed
 				if( command.completed ) {
 					delete self.queue[command.commandID];
@@ -94,32 +95,21 @@ function DbConnection( options )
 
 DbConnection.prototype.readPacket = function()
 {
-	var pos = -1;
 	var packet = null;
-
-	// Check if we have a full packet inside the buffer (either header or data)
-	if( this.buffer.match( /^\d+ \w+$/m ) ) {
-		// We are receiving the headers
-		pos = this.buffer.indexOf( '\r\n\r\n' );
-		if( pos != -1 ) {
-			// If so, extract the packet from the buffer
-			packet = this.buffer.slice( 0, pos );
-			this.buffer = this.buffer.substr( pos + 4 );
+	
+	// Read the first 32 bytes as an ascii string so we can have a peak at the data
+	var string = this.buffer.toString( 'ascii', 0, 32 );
+	var matches = [];
+	
+	// Check if we have received headers or row data
+	if( matches = string.match( /^(\d+) (\w+)$/m ) ) {
+		// Check if the buffer contains a complete header
+		if( this.buffer.indexOf( kCRLF + kCRLF ) != -1 ) {
+			packet = { type: 'headers', commandID: matches[1] };
+			this.lastCommandID = packet.commandID;
 		}
-	} else if( this.buffer.match( kCRLF ) ) {
-		// We are receiving the rows
-		if( this.buffer.endsWith( '\r' ) ) {
-			// 4D ends all rows with CRLF except the last row
-			// Fix this, by appending a LF
-			this.buffer += '\n';
-		}
-		pos = this.buffer.lastIndexOf( '\r\n' );
-		if( pos != -1 ) {
-			// If so, extract the packet from the buffer
-			pos += 2;	// Include the CRLF for the rows
-			packet = this.buffer.slice( 0, pos );
-			this.buffer = this.buffer.substr( pos );
-		}
+	} else if( string.match( /^[0|1|2]/m ) ) {
+		packet = { type: 'data', commandID: this.lastCommandID };
 	}
 
 	return packet;
@@ -162,7 +152,7 @@ DbConnection.prototype.connect = function( callback )
 				this.onCompleteHandler( null );
 			} else {
 				console.log( 'Node-4D: Login failed' );
-				this.onCompleteHandler( this.result.errors );
+				this.onCompleteHandler( this.errors );
 			}
 		}
 		
@@ -221,7 +211,7 @@ DbConnection.prototype.query = function( sql, params, callback )
 
 	var command = this.createCommand( 'EXECUTE-STATEMENT' );
 	command.request += 'STATEMENT:' + sql + kCRLF;
-	command.request += 'OUTPUT-MODE:Debug' + kCRLF;
+	command.request += 'OUTPUT-MODE:Release' + kCRLF;
 	command.request += 'PREFERRED-IMAGE-TYPES:jpg png' + kCRLF;
 	command.request += 'FIRST-PAGE-SIZE:' + this.fetchLimit + kCRLF;
 	command.request += 'FULL-ERROR-STACK:Y' + kCRLF;
@@ -231,19 +221,18 @@ DbConnection.prototype.query = function( sql, params, callback )
 
 	command.onDataHandler = function( data ) {
 		if( Array.isArray( data ) == false  ) {
-			if( this.result.errors ) {
+			if( this.errors ) {
 				this.completed = true;
-				this.onCompleteHandler( this.result.errors, null, null );
+				this.onCompleteHandler( this.errors, null, null );
 			}
 		} else {
-			this.setRows( data );
 			// Check if we need to fetch more rows
 			if( this.result.rowCountReceived == this.result.rowCountSent && this.result.rows.length < this.result.rowCount ) {
 				self.fetch( this.result, this.onCompleteHandler );
-			} else if( this.result.rows.length == this.result.rowCount ) {
+			} else if( this.result.rows.length >= this.result.rowCount ) {
 				// We have fetched all rows, run the on complete handler
 				this.completed = true;
-				this.onCompleteHandler( this.result.errors, this.result.rows, this.result.fields );
+				this.onCompleteHandler( this.errors, this.result, this.result.fields );
 			}
 		}
 	}
@@ -267,7 +256,7 @@ DbConnection.prototype.fetch = function( result, callback )
 	var command = this.createCommand( 'FETCH-RESULT' );
 	command.request += 'STATEMENT-ID:' + result.statementID + kCRLF;
 	command.request += 'COMMAND-INDEX:' + commandIndex + kCRLF;
-	command.request += 'OUTPUT-MODE:Debug' + kCRLF;
+	command.request += 'OUTPUT-MODE:Release' + kCRLF;
 	command.request += 'FIRST-ROW-INDEX:' + firstRow + kCRLF;
 	command.request += 'LAST-ROW-INDEX:' + lastRow + kCRLF;
 	command.request += 'FULL-ERROR-STACK:Y' + kCRLF;
@@ -275,9 +264,9 @@ DbConnection.prototype.fetch = function( result, callback )
 	
 	command.onDataHandler = function( data ) {
 		if( Array.isArray( data ) == false ) {
-			if( this.result.errors ) {
+			if( this.errors ) {
 				this.completed = true;
-				this.onCompleteHandler( this.result.errors, null, null );
+				this.onCompleteHandler( this.errors, null, null );
 			}
 		} else {
 			this.setRows( data );
@@ -287,7 +276,7 @@ DbConnection.prototype.fetch = function( result, callback )
 			} else if( this.result.rows.length == this.result.rowCount ) {
 				// We have fetched all rows, run the on complete handler
 				this.completed = true;
-				this.onCompleteHandler( this.result.errors, this.result.rows, this.result.fields );
+				this.onCompleteHandler( this.errors, this.result, this.result.fields );
 			}
 		}
 	}
@@ -315,124 +304,13 @@ function DbCommand( id, type )
 	this.completed = false;
 }
 
-DbCommand.prototype.setResponseHeaders = function( headers )
-{
-	var result = new DbResultSet();
-	this.result = result;
-	
-	if( headers.status == 'ERROR' ) {
-		result.errors = {};
-		result.errors.code = Number( headers.ErrorCode );
-		result.errors.message = headers.ErrorDescription;
-		if( headers.StackError1 )
-			result.errors.message += '\n' + headers.StackError1;
-		if( headers.StackError2 )
-			result.errors.message += '\n' + headers.StackError2;
-		if( headers.StackError3 )
-			result.errors.message += '\n' + headers.StackError3;
-		return;
-	}
-	
-	result.resultType = headers.ResultType || 'ERROR';
-	if( headers.ResultType == 'Result-Set' ) {
-		result.statementID = headers.StatementID;
-		result.commandCount = Number( headers.CommandCount );
-		result.columnCount = Number( headers.CommandCount );
-		result.rowCount = Number( headers.RowCount );
-		result.rowCountSent = Number( headers.RowCountSent );
-		result.rowCountReceived = 0;
-		result.fields = [];
-		result.rows = [];
-		for( var i = 0 ; i < headers.ColumnTypes.length ; i++ ) {
-			result.fields.push( {
-				name: headers.ColumnAliases[i],
-				type: headers.ColumnTypes[i],
-				updatable: ( headers.ColumnUpdateability[i] == 'Y' )} 
-			);
-		}
-	} else if( result.resultType == 'Update-Count' ) {
-		
-	}
-}
-
-DbCommand.prototype.setRows = function( rows )
-{
-	for( var i = 0 ; i < rows.length ; i++ ) {
-		var row = {};
-		for( var j = 0 ; j < this.result.fields.length ; j++ ) {
-			var field = this.result.fields[j].name;
-			var type = this.result.fields[j].type;
-			var value = rows[i][j];
-			// Convert the value to its closest equivalent
-			switch( type )
-			{
-				case 'VK_STRING' :
-				case 'VK_TEXT' :
-					break;
-				case 'VK_LONG' :
-				case 'VK_LONG8' :
-				case 'VK_BYTE' :
-				case 'VK_WORD' :
-				case 'VK_REAL' :
-				case 'VK_FLOAT' :
-					value = Number( value );
-					break;
-				case 'VK_BOOLEAN' :
-					value = ( value == 'true' );
-					break;
-				case 'VK_TIME' :
-				case 'VK_TIMESTAMP' :
-					// Is date- time value
-					var items = value.match( /(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})/ );
-					if( items && items.length >= 6 ) {
-						value = new Date( items[3] + '-' + items[2] + '-' + items[1] + ' ' + items[4] + ':' + items[5] );
-					} else {
-						value = null;
-					}
-					break;
-				case 'VK_DURATION' :
-					// Is time-only value (do not translate)
-					break;
-				case 'VK_BLOB' :
-				case 'VK_IMAGE' :
-					value = value ? Buffer.from( value, 'base64' ) : null;
-					break;
-			}
-			row[field] = value;
-		}
-		this.result.rows.push( row );
-	}
-
-	this.result.rowCountReceived += rows.length;
-}
-
-function DbResultSet()
-{
-	this.resultType = '';
-	this.statementID = 0;
-	this.commandCount = 0;
-	this.columnCount = 0;
-	this.rowCount = 0;
-	this.errors = null;
-	this.rows = [];
-	this.fields = [];
-	this.lastInsertID = 0;
-	this.affectedRows = 0;
-}
-
-// Utilities
-
-function md5( value )
-{
-	var hash = crypto.createHash('md5').update( value ).digest('hex');
-	return hash;
-}
-
-function parseHeaders( packet )
+DbCommand.prototype.parseHeaders = function( buffer )
 {
 	var headers = {};
+	var offset = buffer.indexOf( kCRLF + kCRLF );
+	var packet = buffer.toString( 'utf8', 0, offset );
 	
-	if( packet.match( /^\d+ \w+$/m ) ) {
+	if( packet.match( /^\d+ \w+$/m ) && offset != 0 ) {
 		// Define a list of headers that contain lists
 		const listTypes = [ 'Column-Types', 'Column-Aliases', 'Column-Updateability' ];
 		var lines = packet.split( '\r\n' );
@@ -472,11 +350,219 @@ function parseHeaders( packet )
 			}
 			headers[key] = value;
 		}
+		
+		offset += ( 2 * kCRLF.length );
 	} else {
 		headers = null;
 	}
 	
-	return headers;
+	return [ headers, offset ];
+}
+
+DbCommand.prototype.setResponseHeaders = function( headers )
+{
+	var result = new DbResultSet();
+	this.result = result;
+	this.result.headers = headers;
+	console.log( headers );
+	if( headers.status == 'ERROR' ) {
+		this.errors = {};
+		this.errors.code = Number( headers.ErrorCode );
+		this.errors.message = headers.ErrorDescription;
+		if( headers.StackError1 )
+			this.errors.message += '\n' + headers.StackError1;
+		if( headers.StackError2 )
+			this.errors.message += '\n' + headers.StackError2;
+		if( headers.StackError3 )
+			this.errors.message += '\n' + headers.StackError3;
+		return;
+	}
+	
+	result.resultType = headers.ResultType || 'ERROR';
+	if( headers.ResultType == 'Result-Set' || headers.ResultType == 'Update-Count' ) {
+		result.statementID = headers.StatementID;
+		result.commandCount = Number( headers.CommandCount );
+		result.columnCount = Number( headers.ColumnCount );
+		result.rowCount = Number( headers.RowCount );
+		result.rowCountSent = Number( headers.RowCountSent );
+		result.rowCountReceived = 0;
+		result.fields = [];
+		result.rows = [];
+		for( var i = 0 ; i < headers.ColumnTypes.length ; i++ ) {
+			result.fields.push( {
+				name: headers.ColumnAliases[i],
+				type: headers.ColumnTypes[i],
+				updatable: ( headers.ColumnUpdateability[i] == 'Y' )} 
+			);
+		}
+	}
+}
+
+DbCommand.prototype.parseRows = function( buffer )
+{
+	var offset = 0;
+	var result = [];
+	var offsetLastRecord = 0;
+	
+	// Fill an array with the column names and types
+	var names = this.result.headers.ColumnAliases.slice();
+	var types = this.result.headers.ColumnTypes.slice();
+	// If the rowset is updateable, then the first item in the data is the row number
+	if( this.result.headers.ColumnUpdateability.indexOf( 'Y' ) != -1 ) {
+		names.unshift( '__RECORDNR__' );
+		types.unshift( 'VK_LONG' );
+	}
+
+	while( offset < buffer.length ) {
+		
+		var record = {};
+		var columnCount = 0;
+		
+		try {
+			for( var colNr = 0 ; colNr < types.length ; colNr++ ) {
+				
+				if( offset >= buffer.length) {
+					break;
+				}
+				
+				var name = names[colNr];
+				var type = types[colNr];
+				var length = 0;
+				var value, error;
+				var status = buffer.toString( 'ascii', offset, offset + 1 );
+				offset++;
+				
+				switch( status ) {
+					case '0' :
+						value = null;
+						break;
+					case '1' :
+						switch( type ) {
+							case 'VK_STRING' :
+							case 'VK_TEXT' :
+								length = Math.abs( buffer.readInt32LE( offset ) );
+								offset += 4;
+								length *= 2;
+								value = buffer.toString( 'utf16le', offset, offset + length );
+								offset += length;
+								break;
+							case 'VK_LONG' :
+								value = buffer.readInt32LE( offset );
+								offset += 4;
+								break;
+							case 'VK_BYTE' :
+								value = buffer.readInt8( offset );
+								offset += 1;
+								break;
+							case 'VK_WORD' :
+								value = buffer.readInt16LE( offset );
+								offset += 2;
+								break;
+							case 'VK_LONG8' :
+								// Note: JavaScript uses a 64-bit double for numbers, which does not cover the entire range of a 64-bit int
+								value = ( buffer.readInt32LE( offset ) << 8 ) + buffer.readInt32LE( offset + 4 );
+								offset += 8;
+								break;
+							case 'VK_REAL' :
+								value = buffer.readDoubleLE( offset );
+								offset += 8;
+								break;
+							case 'VK_FLOAT' :
+								value = buffer.readDoubleLE( offset );
+								offset += 4;
+								break;
+							case 'VK_BOOLEAN' :
+								value = buffer.readInt16LE( offset );
+								offset += 2;
+								break;
+							case 'VK_TIME' :
+							case 'VK_TIMESTAMP' :
+								var year = buffer.readInt16LE( offset );
+								offset += 2;
+								var month = buffer.readInt8( offset );
+								offset += 1;
+								var day = buffer.readInt8( offset );
+								offset += 1;
+								var seconds = buffer.readInt32LE( offset );
+								offset += 4;
+								value = new Date( year, month - 1, day, 0, 0, seconds );
+								break;
+							case 'VK_DURATION' :
+								value = buffer.readInt32LE( offset );
+								offset += 4;
+								break;
+							case 'VK_BLOB' :
+							case 'VK_IMAGE' :
+								length = buffer.readInt32LE( offset );
+								offset += 4;
+								value = buffer.slice( offset, offset + length );
+								offset += length;
+								break;
+							default :
+								error = "Unknown data-type: " + type;
+								throw error;
+								break;
+						}
+						break;
+					case '2' :
+						error = "Error in data from stream: " + buffer.readInt32LE( offset );
+						offset += 4;
+						throw error;
+						break;
+					default :
+						error = "Unknown status byte: " + status;
+						throw error;
+						break;
+				}
+				
+				record[name] = value;
+				columnCount++;
+			}
+			
+			// If we have all the expected columns, then add the record to the result
+			if( columnCount == types.length ) {
+				result.push( record );
+				offsetLastRecord = offset;
+			}
+		} catch( error ) {
+			if( error instanceof RangeError ) {
+				// We have exceeded the bounds of the buffer (an incomplete row), exit the loop
+				break;
+			} else {
+				throw error;
+			}
+		}
+	}
+	
+	return [ result, offsetLastRecord ];
+}
+
+DbCommand.prototype.setRows = function( rows )
+{	
+	this.result.rows = this.result.rows.concat( rows );
+	this.result.rowCountReceived += rows.length;
+}
+
+function DbResultSet()
+{
+	this.resultType = '';
+	this.statementID = 0;
+	this.commandCount = 0;
+	this.columnCount = 0;
+	this.rowCount = 0;
+	this.errors = null;
+	this.rows = [];
+	this.fields = [];
+	this.lastInsertID = 0;
+	this.affectedRows = 0;
+}
+
+// Utilities
+
+function md5( value )
+{
+	var hash = crypto.createHash('md5').update( value ).digest('hex');
+	return hash;
 }
 
 function splitList( value )
@@ -489,24 +575,6 @@ function splitList( value )
 		array = value.split( ' ' );
 	}
 	
-	return array;
-}
-
-function parseRows( buffer )
-{
-	// Split the text into rows
-	var lines = buffer.split( kCRLF );
-
-	// Remove the last line if it is empty
-	if( lines[lines.length-1] == '' ) {
-		lines.pop();
-	}
-
-	// Split the rows into columns
-	var array = lines.map( function( line ) {
-		return line.split( '\t' );
-	} );
-
 	return array;
 }
 
